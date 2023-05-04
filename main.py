2b26237843ce77f32d0287e4ade36095250a2901
SiddharthdataScientist <siddharthpujari108@gmail.com>
2023-05-04 16:49:05 +0530

    Updated Code

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from datetime import datetime,date
 from fastapi.middleware.cors import CORSMiddleware
 
 
-
 app = FastAPI()
 
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,6 +24,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 path = "C:/Users/Dell/Documents/fastapi/Files"
 
@@ -43,88 +45,48 @@ class FileRequest(BaseModel):
     file_name : str 
     location : str
 
-def get_files():
-    ## Declare th folder name here
-    ## Get the filenames here
-    # declare the date
-    ##Insert into the table with all the above data
-    return "files"
+# def get_files(filereq, db):
+#     folder_name = "Songs"
+#     folder_path = os.path.expanduser("~/Desktop")+ "/" + folder_name
+#     files = os.listdir(folder_path)
+#     date = datetime.now()
+#     print(files)
+#     filereq.__dict__.update(
+#         date_= date,
+#         file_name = folder_name
+#         location = folder_path
+#     )
+#     existing_file = db.query(File).filter(File.file_name == folder_name).all()
 
-@app.get("/get_files")
-async def read_all_file_names(folder_name:str):
-    folder_path = os.path.expanduser("~/Desktop")+ "/" + folder_name
-    files = os.listdir(folder_path)
-    return {"files":files}
-
-
-
-@app.get("/read_file")
-async def read_file(db:db_dependency):
-    now = datetime.now()
-    filename = "MP3-1.mp3"
-    location = os.path.abspath(filename)
-    existing_file = db.query(File).filter(File.file_name == filename).first()
-    # if existing_file:
-    #     existing_file.location = location
-    #     existing_file.date = now
-    #     db.commit()
-    #     db_data = db.query(File).all()
-    #     return existing_file, db_data
-    # else:
-    #     file_data =  File(file_name = filename,location = location, date = now)
-    #     db.add(file_data)
-    #     db.commit()
-    #     db_data = db.query(File).all()
-    #     print(db_data)
-    #     return file_data, db_data
-    return existing_file
-
-    
-    
-@app.get("/read_files")
-async def read_file(db:db_dependency):
-    now = datetime.now()
-    filename = "MP3-2.mp3"
-    location = os.path.abspath(filename)
-
-    file_data =  File(file_name = filename, location = location, date = now)
-    return file_data
-    # db.add(file_data)
-    # db.commit()
-    # db_data = db.query(File).all()
-    # # return file_data, db_data
-    # # return db_data
+#     return existing_file
 
 
-@app.put("/file/{file_name}")
-async def update_file_name(file_id : int, file_name: str, db:db_dependency):
+
+@app.get("/")
+async def update_table(db:db_dependency):
+    location = location = os.path.expanduser("~/Desktop/Songs")
+    date = datetime.now()
+    list_files = db.query(File.file_name).filter(File.location == location).all()
+    list_files = set([f[0] for f in list_files])
+    for filename in os.listdir(location):
+        if filename in list_files:
+            continue
+        else:
+            file = file(file_name =filename, location = location, date =date)
+            db.add(file)
+    db.commit()
+
+
+@app.put("/files/{update_file_name}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_file_name(file_id: int , db: db_dependency, filename: str):
     file_model = db.query(File).filter(File.id == file_id).first()
-    if file_model is None:
-        raise HTTPException(status_code=404, detail='File not found')
-    
-    file_model.file_name = file_name
-    
-    # db.add(file_model)
-    db.commit()
-    update_filename = db.query(File).filter(File.id == file_id).first()
-    return update_filename
 
-@app.put("/files/{file_name}")
-async def update_file_name(filename: str, db: db_dependency, file_request: FileRequest):
-    file_model = db.query(File).filter(File.file_name == filename).first()
+    file_model.file_name = filename
 
-    file_model.file_name = file_request.file_name
-
-    # db.add(file_model)
     db.commit()
 
 
-
-
-
-
-
-@app.delete("/file/{file_name}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/files/{delete_file}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_file(file_id : int, file_name: str, db:db_dependency):
     file_model = db.query(File).filter(File.id == file_id).first()
     if file_model is None:
@@ -132,6 +94,119 @@ async def delete_file(file_id : int, file_name: str, db:db_dependency):
     
     db.query(File).filter(File.id == file_id).delete()
     db. commit()
+
+
+
+
+
+
+# @app.get("/read_files/{file_name}")
+# async def file_name(Songs:str):
+#     location = os.path.expanduser("~/Desktop") + "/" + Songs
+#     file_name = os.listdir(location)
+#     date = datetime.now()
+#     file_info = {
+#         "files": file_name,
+#         "location": location,
+#         "date" : date
+#     }
+#     return file_info
+
+
+
+
+# 68:
+# db_filename = db.query(File.file_name).filter(File.location == directory).all()
+
+
+
+
+# @app.put("/files/{file_name}")
+# async def file_name(file_name:str, db:db_dependency):
+#     file_model = db.query(File).filter(File.file_name == file_name).first()
+#     file_model.file_name = file_name
+#     db.commit()
+#     # update_file_name = db.query(File).filter(File.file_name==file_name).first()
+#     # return
+
+
+
+
+
+# @app.get("/read_file")
+# async def read_file(db:db_dependency):
+#     now = datetime.now()
+#     filename = "MP3-1.mp3"
+#     location = os.path.abspath(filename)
+#     existing_file = db.query(File).filter(File.file_name == filename).first()
+# #     # if existing_file:
+#     #     existing_file.location = location
+#     #     existing_file.date = now
+#     #     db.commit()
+#     #     db_data = db.query(File).all()
+#     #     return existing_file, db_data
+#     # else:
+#     #     file_data =  File(file_name = filename,location = location, date = now)
+#     #     db.add(file_data)
+#     #     db.commit()
+#     #     db_data = db.query(File).all()
+#     #     print(db_data)
+#     #     return file_data, db_data
+#     return existing_file
+
+    
+    
+# @app.get("/read_files")
+# async def read_file(db:db_dependency):
+#     now = datetime.now()
+#     filename = "MP3-2.mp3"
+#     location = os.path.abspath(filename)
+
+#     file_data =  File(file_name = filename, location = location, date = now)
+#     return file_data
+# #     # db.add(file_data)
+# #     # db.commit()
+#     # db_data = db.query(File).all()
+#     # # return file_data, db_data
+#     # # return db_data
+
+
+# @app.put("/file/{file_name}")
+# async def update_file_name(file_id : int, file_name: str, db:db_dependency):
+#     file_model = db.query(File).filter(File.id == file_id).first()
+#     if file_model is None:
+#         raise HTTPException(status_code=404, detail='File not found')
+    
+#     file_model.file_name = file_name
+    
+#     # db.add(file_model)
+#     db.commit()
+#     update_filename = db.query(File).filter(File.id == file_id).first()
+#     return update_filename
+
+# # @app.put("/files/{file_name}")
+# # async def update_file_name(filename: str, db: db_dependency):
+# #     file_model = db.query(File).filter(File.file_name == filename).first()
+
+# #     file_model.file_name = filename
+
+# #     # db.add(file_model)
+# #     db.commit()
+
+
+
+
+
+
+
+# @app.delete("/file/{file_name}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_file(file_id : int, file_name: str, db:db_dependency):
+#     file_model = db.query(File).filter(File.id == file_id).first()
+#     if file_model is None:
+#         raise HTTPException(status_code=404, detail='File not found')
+    
+#     db.query(File).filter(File.id == file_id).delete()
+#     db. commit()
 
 
 
--- models.py
+++ models.py
@@ -5,13 +5,9 @@ from sqlalchemy import Integer, String, Column, DateTime
 class File(Base):
     __tablename__ = 'project'
     
-    id = Column(Integer, primary_key=True, autoincrement= True)
+    id = Column(Integer, primary_key=True)
     date = Column(DateTime)
     file_name = Column(String, unique=True)
     location = Column(String)
 
-
-    # id = Column(Integer, primary_key=True, index=True)
-    # date_ = Column(String)
-    # filename = Column(String)
 
